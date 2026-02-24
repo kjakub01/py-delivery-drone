aadd8134abe78db565bc6777b58bad322fc598e3
Jakub <jakub.kukier01@gmail.com>
2026-02-24 16:15:56 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -2,4 +2,66 @@ class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
-# write your code here
+
+class BaseRobot(Cargo):
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list[int] = None
+    ) -> None:
+        super().__init__(weight)
+        self.name = name
+        self.coords = coords if coords is not None else [0, 0]
+
+    def go_forward(self, steps: int = 1) -> None:
+        self.coords[1] += steps
+
+    def go_back(self, steps: int = 1) -> None:
+        self.coords[1] -= steps
+
+    def go_left(self, steps: int = 1) -> None:
+        self.coords[0] -= steps
+
+    def go_right(self, steps: int = 1) -> None:
+        self.coords[0] += steps
+
+    def get_info(self) -> str:
+        return f"Robot: {self.name}, Weight: {self.weight}"
+
+
+class FlyingRobot(BaseRobot):
+    def __init__(
+            self,
+            name: str,
+            weight: int,
+            coords: list[int] = None
+    ) -> None:
+        super().__init__(name, weight)
+        self.coords = coords if coords is not None else [0, 0, 0]
+
+    def go_up(self, z_axis: int = 1) -> None:
+        self.coords[2] += z_axis
+
+    def go_down(self, z_axis: int = 1) -> None:
+        self.coords[2] -= z_axis
+
+
+class DeliveryDrone(FlyingRobot):
+    def __init__(
+            self,
+            max_load_weight: int,
+            weight: int,
+            name: str,
+            current_load: Cargo = None,
+            coords: list[int] = None) -> None:
+        super().__init__(name, weight, coords)
+        self.max_load_weight = max_load_weight
+        self.current_load = current_load
+
+    def hook_load(self, cargo: "Cargo") -> None:
+        if self.current_load is None and cargo.weight <= self.max_load_weight:
+            self.current_load = cargo
+
+    def unhook_load(self) -> None:
+        self.current_load = None
